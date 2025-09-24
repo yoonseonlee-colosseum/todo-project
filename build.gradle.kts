050ba1b1e0a963ae987530222731d2757c76e936
이윤선 <yslee@colosseum.kr>
2025-09-24 19:03:23 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,59 +2,61 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm")
-	kotlin("kapt")
-	kotlin("plugin.spring") apply false
-	kotlin("plugin.jpa") apply false
-	id("org.springframework.boot") apply false
-	id("io.spring.dependency-management") apply false
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.spring") apply false
+    kotlin("plugin.jpa") apply false
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management") apply false
+    id("com.colosseum.code-quality") version "1.0.7"
+    id("com.diffplug.spotless") version "6.25.0" apply false // 버전만 명시
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 val projectGroup: String by project
 val applicationVersion: String by project
 
 allprojects {
-	group = projectGroup
-	version = applicationVersion
+    group = projectGroup
+    version = applicationVersion
 
-	repositories {
-		mavenCentral()
-		maven {
-			url = uri("https://maven.pkg.github.com/yoonseonlee-colosseum/todo-library")
-			credentials {
-				username = project.findProperty("gpr.user") as String
-				password = project.findProperty("gpr.key") as String
-			}
-		}
-	}
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/yoonseonlee-colosseum/todo-library")
+            credentials {
+                username = project.findProperty("gpr.user") as String
+                password = project.findProperty("gpr.key") as String
+            }
+        }
+    }
 }
 
 subprojects {
-	apply(plugin = "org.jetbrains.kotlin.jvm")
-	apply(plugin = "org.jetbrains.kotlin.kapt")
-	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-	val p6spyVersion: String by project.extra
+    val p6spyVersion: String by project.extra
 
-	dependencies {
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:$p6spyVersion")
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:$p6spyVersion")
 
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	}
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    }
 
-	tasks.withType<KotlinCompile> {
-		compilerOptions {
-			freeCompilerArgs.set(listOf("-Xjsr305=strict"))
-			jvmTarget.set(JvmTarget.JVM_21)
-		}
-	}
+    tasks.withType<KotlinCompile> {
+        compilerOptions {
+            freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,40 @@
 rootProject.name = "skeleton"
 
 include(
-	"skeleton-api",
-	"skeleton-domain",
-	"skeleton-infra",
+    "skeleton-api",
+    "skeleton-domain",
+    "skeleton-infra",
 )
 
 pluginManagement {
-	val kotlinVersion: String by settings
-	val springBootVersion: String by settings
-	val springDependencyManagementVersion: String by settings
-	val querydslVersion: String by settings
-	val p6spyVersion: String by settings
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val querydslVersion: String by settings
+    val p6spyVersion: String by settings
 
-	resolutionStrategy {
-		eachPlugin {
-			when (requested.id.id) {
-				"org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
-				"org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
-				"org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
-				"org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
-				"org.springframework.boot" -> useVersion(springBootVersion)
-				"io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
-			}
-		}
-	}
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/yoonseonlee-colosseum/colosseum-libs")
+            credentials {
+                username = ""
+                password = ""
+            }
+        }
+    }
 }
